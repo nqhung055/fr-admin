e0d79543cb673c8539e789647db87c1bda4aa6a3
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-11 14:13:27 +0700

    Update VisitorsCollection.vue

--- src/views/fr-charts/VisitorsCollection.vue
+++ src/views/fr-charts/VisitorsCollection.vue
@@ -281,15 +281,16 @@ export default {
           //   sumRU += item.noStranger; sumG += item.noUser;
           //   console.log('key: ' + key + ' - item: ' + item.label + ' - noStranger: ' + item.noStranger + ' - noUser: ' + item.noUser);
           // }
-         Object.entries(response.data).forEach(item => {
-            listLabels.push( item.label); listRUPoints.push( item.noStranger); listGPoints.push( item.noUser);
-            sumRU += item.noStranger; sumG += item.noUser;
-            console.log(' - label: ' + item.label + ' - noStranger: ' + item.noStranger + ' - noUser: ' + item.noUser);
+          Object.entries(response.data).forEach((key) => {
+            console.log('key: ' + key.label);
+            // listLabels.push( item.label); listRUPoints.push( item.noStranger); listGPoints.push( item.noUser);
+            // sumRU += item.noStranger; sumG += item.noUser;
+            // console.log(' - label: ' + item.label + ' - noStranger: ' + item.noStranger + ' - noUser: ' + item.noUser);
           });
           this.sumRUPoints = sumRU; this.sumGPoints = sumG; this.arrLabels = listLabels; this.RUPoints = listRUPoints; this.GPoints = listGPoints;
-          this.arrLabels.forEach(elm => {
-            console.log('Label: ' + elm);
-          })
+          // this.arrLabels.forEach(elm => {
+          //   console.log('Label: ' + elm);
+          // })
           // console.log('listLabels: ' + listLabels.length + ' - listRUPoints: ' + listGPoints.length + ' : ' + this.sumRUPoints + ' - listGPoints: ' + listGPoints.length + ' : ' + this.sumGPoints);
           // this.dayTotalVisitors = sumEmp + sumGuest;
           // this.lstVisitors = response.data;
