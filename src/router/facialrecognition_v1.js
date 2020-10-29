b994853268cbb7a057aad0e7c33fddf73802293a
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-29 10:56:07 +0700

    HungDQ update

--- src/router/facialrecognition_v1.js
+++ src/router/facialrecognition_v1.js
@@ -19,7 +19,7 @@ const Visitors_Statistics = () => import('Views/dashboard/Visitor-Statistics');
 
 const Users_List = () => import('Views/fr-users/Users');
 
-const Report = () => import('Views/fr-reports/Reports');
+const Report = () => import('Views/fr-reports/Reports-V1');
 const DeviceManagement = () => import('Views/fr-devices/Devices-List');
 const BlockManagement = () => import('Views/fr-blocks/BlockManagement');
 const CompanyManagement = () => import('Views/fr-companies/CompanyManagement');
