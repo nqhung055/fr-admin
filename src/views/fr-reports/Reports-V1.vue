01e91414accc03baa612ee25f6ba4ce5ed8c4ab2
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-11-01 05:24:41 +0700

    Update Reports-V1.vue

--- src/views/fr-reports/Reports-V1.vue
+++ src/views/fr-reports/Reports-V1.vue
@@ -18,14 +18,43 @@
               <div v-else>
                 <v-card-text>
                   <v-row>
+                    <v-col cols="5" sm="3" md="2" lg="5" xl="1">
+                      <label>Slect Date range: </label>
+                      <date-range-picker
+                        ref="picker"
+                        :opens="opens"
+                        :locale-data="locale"
+                        :ranges="ranges"
+                        :singleDatePicker="singleDatePicker"
+                        :timePicker="timePicker"
+                        :secondPicker="secondPicker"
+                        :timePicker24Hour="timePicker24Hour"
+                        :showWeekNumbers="showWeekNumbers"
+                        :showDropdowns="showDropdowns"
+                        :autoApply="autoApply"
+                        v-model="dateRange"
+                        @update="getLogs(dateRange.startDate, dateRange.endDate, !gateType ? '' : gateType, !userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId)"
+                        @toggle="checkOpen"
+                        :linkedCalendars="linkedCalendars"
+                        clearable
+                      >
+                        <template v-slot:input="picker" style="min-width: 200px;">
+                            {{ picker.startDate | date }} - {{ picker.endDate | date }}
+                        </template>
+                      </date-range-picker>
+                      <v-btn text @click="drClear(); getLogs(dateRange.startDate, dateRange.endDate, !gateType ? '' : gateType, !userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId);" >Clear</v-btn>
+                    </v-col>
+                    <v-spacer></v-spacer>
+                  </v-row>
+                  <v-row>
                     <v-col cols="3">
-                      <v-select v-model="userType" :items="userTypes" item-text="name" item-value="key" label="Select User Types" @change="getLogs(!userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId)" />
+                      <v-select v-model="userType" :items="userTypes" item-text="name" item-value="key" label="Select User Types" @change="getLogs(!dateRange.startDate ? new Date() : dateRange.startDate, !dateRange.endDate ? new Date() : dateRange.endtDate, !gateType ? '' : gateType, !userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId)" />
                     </v-col>
                     <v-col cols="3" class="header-table-select-devices">
-                      <v-select v-model="selectedDevice" :items="devices" item-text="displayName" item-value="name" label="Select Device" @change=" getLogs(); siteId = '-- Please select site --'; blockId = '-- Please select block --'; floorId = '-- Please select floor --'; compId = '-- Please select company --';" />
+                      <v-select v-model="selectedDevice" :items="devices" item-text="displayName" item-value="name" label="Select Device" @change=" getLogs(); gateType = 'all'; siteId = '-- Please select site --'; blockId = '-- Please select block --'; floorId = '-- Please select floor --'; compId = '-- Please select company --';" />
                     </v-col>
                     <v-col cols="3">
-                      <v-select v-model="gateType" :items="gateTypes" item-text="name" item-value="key" label="Select Entry/Exit" @change="getLogs(!userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId)" />
+                      <v-select v-model="gateType" :items="gateTypes" item-text="name" item-value="key" label="Select Entry/Exit" @change="getLogs(!dateRange.startDate ? new Date() : dateRange.startDate, !dateRange.endDate ? new Date() : dateRange.endtDate, !gateType ? '' : gateType, !userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId)" />
                     </v-col>
                     <v-col cols="3" class="header-table-search">
                       <v-text-field v-model="searchUserKey" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
@@ -77,13 +106,9 @@
                         @change="getLogs(!userType ? '' : userType, !siteId ? '' : siteId, !blockId ? '' : blockId, !floorId ? '' : floorId, !compId ? '' : compId);"></v-select>
                     </v-col>
                   </v-row>
+                  
                 </v-card-text>
-                <v-data-table
-                  :headers="headers"
-                  :items="reports"
-                  :search="searchUserKey"
-                  item-key=""
-                >
+                <v-data-table :headers="headers" :items="reports" :search="searchUserKey" item-key="id">
                   <template v-slot:item="{ item }">
                     <tr>
                       <!-- <td>
@@ -92,8 +117,10 @@
                             .map(function (x) {return x.id;})
                             .indexOf(item.id)
                         }}
-                      </td> -->
-                      <td>{{ new Date(item.detectionTime).toTimeString().substr(0,8) }}</td>
+                      </td>
+                      .toTimeString().substr(0,8)
+                      -->
+                      <td>{{moment(item.detectionTime)}}</td>
                       <td><img :src="item.photo" class="img-responsive" height="50" width="50" /></td>
                       <td>{{ item.bodyTemperature > 37.5 ? `FAIL` : 35.5 > item.bodyTemperature ? `FAIL` : 'PASS' }}</td>
                       <!-- <td>{{ 35.5 > item.bodyTemperature ? `FAIL` : `PASS` }}</td> -->
@@ -148,10 +175,17 @@
 <script>
 import Vue from "vue";
 import AppConfig from "../../constants/AppConfig";
+import moment from 'moment';
+//Vue2  date-range-picker
+import DateRangePicker from 'vue2-daterange-picker'
+//you need to import the CSS manually (in case you want to override it)
+import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
 
 export default {
-  components: {},
+  components: {DateRangePicker},
   data() {
+    let startDate = moment().subtract(6, 'days');
+    let endDate = moment();
     return {
       loader: true,
       loading: true,
@@ -257,7 +291,7 @@ export default {
       showConfirmDeleteUsersDialog: false,
       userType: "all",
       userTypes: [{"key":"all", "name":"All"}, {"key":"registered_user", "name":"Registered users"}, {"key":"stranger", "name":"Guests"}],
-      gateType: "entry",
+      gateType: "all",
       gateTypes: [{"key":"all", "name":"All"}, {"key":"entry", "name":"Entry",}, {"key":"exit", "name":"Exit"}],
       userBlocks: [],
       blockId: "-- Please select block --",
@@ -267,25 +301,67 @@ export default {
       floorId: "-- Please select floor --",
       userCompanies: [],
       compId: "-- Please select company --",
+      //vue2-daterange-picker
+      opens: "center", //which way the picker opens, default "center", can be "left"/"right"
+      locale: {
+        direction: 'ltr',
+        format: 'dd/mm/yyyy hh:mm', // hh:mm:ss
+        separator: ' - ',
+        applyLabel: 'Apply',
+        cancelLabel: 'Cancel',
+        weekLabel: 'W',
+        customRangeLabel: 'Custom Range',
+        daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        firstDay: 0
+      },
+      ranges: { //default value for ranges object (if you set this to false ranges will no be rendered)
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'This month': [moment().startOf('month'), moment().endOf('month')],
+        'This year': [moment().startOf('year'), moment().endOf('year')],
+        'Last week': [moment().subtract(1, 'week').startOf('week'), moment().subtract(1, 'week').endOf('week')],
+        'Last month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+      },
+      secondPicker: true,
+      timePicker: true,
+      timePicker24Hour: true,
+      autoApply: false,
+      singleDatePicker: false,
+      showWeekNumbers: false,
+      showDropdowns: false,
+      linkedCalendars: false,
+      dateRange: {startDate, endDate},
     };
   },
   mounted() {
     this.getDevices();
     this.getUserRelatedData();
+    // const dt = moment('2020-11-01 22:34:08').format('DD/MM/YYYY'); // hh:mm:ss
+    // const dt = new Date('2020-11-1').toDateString(); // hh:mm:ss
+
+    // const  dt1 = moment().format('DD/MM/YYYY');
+    // const dt1 = new Date().toDateString(); // hh:mm:ss
+
+    // console.log('dt: '  + dt + ' - dt1: ' + dt1);
+    // console.log(dt >= dt1);
+
   },
   computed: {
   },
   methods: {
-    async getLogs(userType = "", sId = "", bId = "", fId = "", cId = "") {
+    async getLogs(sd = "", ed = "", userType = "",  gateType = "", sId = "", bId = "", fId = "", cId = "") {
+      // console.log("getLogs: " + this.dateRange.startDate +  ' - ' + this.dateRange.endDate);
       if (this.selectedDevice !== null || this.selectedDevice !== "") {
         // &page=1&pageSize=5&${this.siteName}
         let sDN = !Object.isFrozen(this.selectedDevice) ? this.selectedDevice["name"] : this.selectedDevice;
         let sUserType = userType === "all" ? "" : userType;
+        let sGateType = gateType === "all" ? "" : gateType;
         let sSite = sId === "-- Please select site --" ? "" : sId;
         let sBlock = bId === "-- Please select block --" ? "" : bId;
         let sFloor = fId === "-- Please select floor --" ? "" : fId;
         let sComp = cId === "-- Please select company --" ? "" : cId;
-        let sAPI = `${AppConfig.ip}${AppConfig.api_port}/reports?device=${sDN}${!sUserType ? "" : "&type=" + sUserType}${!sSite ? "" : "&site=" + sSite}${!sBlock ? "" : "&block=" + sBlock}${!sFloor ? "" : "&floor=" + sFloor}${!sComp ? "" : "&comp=" + sComp}`;
+        let sAPI = `${AppConfig.ip}${AppConfig.api_port}/reports?device=${sDN}${!sd ? "" : "&sd="  + moment(sd).format('YYYY-MM-DD hh:mm')}${!ed ? "" : "&ed="  + moment(ed).format('YYYY-MM-DD hh:mm')}${!sUserType ? "" : "&type=" + sUserType}${!sGateType ? "" : "&gate=" + sGateType}${!sSite ? "" : "&site=" + sSite}${!sBlock ? "" : "&block=" + sBlock}${!sFloor ? "" : "&floor=" + sFloor}${!sComp ? "" : "&comp=" + sComp}`;
         // console.log('Log api: ' + sAPI);
         await this.$axios
           .get(sAPI)
@@ -388,7 +464,26 @@ export default {
         () => (this.loading = false);
       }
     },
+    checkOpen() {
+      //console.log("updateValues: " + this.dateRange.startDate +  ' - ' + this.dateRange.endDate);
+    },
+    updateValues() {
+      console.log("updateValues: " + this.dateRange.startDate +  ' - ' + this.dateRange.endDate);
+    },
+    drClear(){
+      this.dateRange.startDate= '';
+      this.dateRange.endDate= '';
+    },
+    moment (date) {
+      return moment(date).format('DD-MM-YYYY hh:mm');
+    },
   },
+  filters: {
+    date(val) {
+      return val ? moment(val).format("DD/MM/YYYY hh:mm") : ''
+      // hh:mm:00 A
+    }
+  }
 };
 </script>
 
