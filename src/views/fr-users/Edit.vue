92f2d14c04d5bb75de4adad2c83d7f63bc9d1b24
lethanhtrung2020 <lethanhtrung2020@gmail.com>
2020-11-01 14:57:08 +0700

    Update Edit.vue

--- src/views/fr-users/Edit.vue
+++ src/views/fr-users/Edit.vue
@@ -468,7 +468,7 @@ export default {
         if (this.isDirtyIc) {
           objUpdateUser = { ...objUpdateUser, icCard: ic}
         } else {
-          objUpdateUser = { ...objUpdateUser, icCard: this.editUserModel.icCard_Expiry}
+          objUpdateUser = { ...objUpdateUser, icCard: this.editUserModel.icCard}
         }
         delete editUser.ic
         const editResponse = await this.$axios.post('/upload/user', editUser)
