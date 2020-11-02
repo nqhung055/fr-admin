945223bcd3a5b3890b16376e6629c90748a778f4
lethanhtrung2020 <lethanhtrung2020@gmail.com>
2020-11-02 10:44:46 +0700

    Update DownloadUsers.vue

--- src/views/fr-users/DownloadUsers.vue
+++ src/views/fr-users/DownloadUsers.vue
@@ -35,14 +35,37 @@
         </v-card-text>
       </v-card>
     </v-dialog>
+    <v-dialog
+      v-model="showConfirmDialog"
+      max-width="400"
+      @click:outside="showConfirmDialog=false"
+    >
+      <v-card>
+        <v-card-title> Clear current user data? </v-card-title>
+        <v-card-text>
+          Do you want clear all current user data in device and upload this user
+          data?
+        </v-card-text>
+        <v-card-actions class="pa-4">
+          <v-spacer></v-spacer>
+          <v-btn class="px-4" color="success" @click="createUsers(true)">
+            Yes
+          </v-btn>
+          <v-btn class="px-4" color="error" @click="createUsers(false)">
+            No
+          </v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
-//import Vue from 'vue';
+import Vue from 'vue';
 // import moment from 'moment';
-//import XLSX from 'xlsx';
-//import AppConfig from '../../constants/AppConfig';
+import XLSX from 'xlsx';
+import AppConfig from '../../constants/AppConfig';
 
 export default {
   props: ['isShowPopup', 'connectedDevices'],
@@ -50,23 +73,24 @@ export default {
     return {
       loading: true,
       errored: false,
-      
-      devicesRules: [
-        devices => !devices || !!devices.length || "Atlease one device is required!"
-      ],
+      isUploadDataValid: true,
+      selectedDevices: [],
       showConfirmDialog: false,
-      
     }
   },
   mounted() {},
   computed: {},
   methods: {
     downloadExcelFiles() {
+      
     },
     closePopup() {
-      this.clearUploadedData()
       this.$emit('closePopup', true)
-    } 
+    },
+    openConfirmDialog() {
+      if (!this.$refs.uploadData.validate()) return
+      this.showConfirmDialog = true
+    },
   }
 }
 </script>
